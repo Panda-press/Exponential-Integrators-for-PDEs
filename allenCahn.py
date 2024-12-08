b83aa8d3dc00a0625673e1322799ea19de8f860f
Harvey <tyrrellhs@gmail.com>
2024-12-08 00:33:40 +0000

    Updated AllenCahn equation

--- allenCahn.py
+++ allenCahn.py
@@ -47,8 +47,15 @@ def test2(gridView):
 """Traveling Wave"""
 def test3(gridView, alpha=0.25):
     tauFE = 0.2
-    c = sqrt(2)*(0.5 - alpha)
     x_val = x[0]
-    exact = lambda t: as_vector([exp((x_val-c*t)/sqrt(2))/(1+exp((x_val-c*t)/sqrt(2)))])
-    dtExact = lambda t: -(1/2 - alpha) * as_vector([exact(t)]) * (as_vector([1]) - exact(t)) 
-    return model(exact, dtExact, lambda u: as_vector([0])), 8, tauFE, exact(0), exact
+
+    exact = lambda t: as_vector([exp(x_val/sqrt(2)+(0.5-alpha)*t)/(1+exp(x_val/sqrt(2)+(0.5-alpha)*t))])
+    #dtExact = lambda t: -(1/2 - alpha) * as_vector([exact(t)]) * (as_vector([1]) - exact(t)) 
+
+    boundary = lambda t: dot(dot(grad(exact(t)[0]),n),v[0])*ds
+    potential = dot(u[0]-alpha,1-u[0]) * dot(u,v) * dx
+    a = inner(grad(u),grad(v))*dx
+
+    #return model(exact, dtExact, lambda u: as_vector([0])), 8, tauFE, exact(0), exact
+    return a - potential, 8, tauFE, exact(0), exact
+
