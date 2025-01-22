4f20876aefd47b31e9854ffd294bab5f05ef25c0
Harvey <tyrrellhs@gmail.com>
2025-01-22 21:34:17 +0000

    Updated Test Params

--- FETester.py
+++ FETester.py
@@ -154,10 +154,10 @@ if __name__ == "__main__":
             exp_methods = ["EXPLAN", "BE", "EXPKIOPS", "EXP1LAN", "EXP2LAN"]
         else:
             tau0 = 8e-1 # Using this value as a higher value such as 8e-2 causes numerical issues
-            taus = 12 #9
-            grids = [[10, 10], [30, 10], [60, 10], [120, 10], [240, 10], [480,10]]
-            exp_methods = ["EXPLAN", "EXPKIOPS", "BE", "EXP1LAN", "EXP2LAN"]
-            krylovSizes = [5, 10, 20]
+            taus = 6 #9
+            grids = [[10, 10], [30, 10], [60, 10]]
+            exp_methods = ["EXPLAN", "BE", "EXP1LAN", "EXP2LAN"]
+            krylovSizes = [5, 10, 20, 40]
     else:
         from parabolicTest import dimR, time, sourceTime, domain
         from parabolicTest import paraTest2 as problem
