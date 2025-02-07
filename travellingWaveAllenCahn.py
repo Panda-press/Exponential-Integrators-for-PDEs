1f933d314650f8ae357c7ea883d604d8b296903b
Harvey <tyrrellhs@gmail.com>
2025-02-07 00:20:18 +0000

    Made boundary condition better

--- travellingWaveAllenCahn.py
+++ travellingWaveAllenCahn.py
@@ -20,8 +20,8 @@ def test3(gridView, alpha=0.25):
     exact = lambda t: as_vector([exp((-x_val/sqrt(2)+(0.5-alpha)*t))/(1+exp((-x_val/sqrt(2)+(0.5-alpha)*t)))])
     #dtExact = lambda t: -(1/2 - alpha) * as_vector([exact(t)]) * (as_vector([1]) - exact(t)) 
           
-    rboundary = DirichletBC(space, [exact(sourceTime)[0]], x[0] > 8 - 1e-30)
-    lboundary = DirichletBC(space, [exact(sourceTime)[0]], x[0] < -8 + 1e-30)
+    rboundary = DirichletBC(space, [exact(sourceTime)[0]], x[0] > 8 - 1e-100)
+    lboundary = DirichletBC(space, [exact(sourceTime)[0]], x[0] < -8 + 1e-100)
     potential = dot(u[0]-alpha,1-u[0]) * dot(u,v) * dx
     a = inner(grad(u),grad(v))*dx
 
