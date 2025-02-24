f956d6c8a7c5064fd6a77eca1b0237954906d24f
Harvey <tyrrellhs@gmail.com>
2025-02-24 13:36:58 +0000

    Snowflake Changed to 6 point star

--- snowflakes3D.py
+++ snowflakes3D.py
@@ -19,7 +19,7 @@ domain = [-width, -width, -depth], [width, width, depth], [25, 25, 4]  # Use a g
 space = Space(3,dimRange=dimR)
 x,u,v,n = ( SpatialCoordinate(space), TrialFunction(space), TestFunction(space), FacetNormal(space) )
 
-u_initial = Constant(0.7, "u_0")
+u_initial = Constant(0.5, "u_0")
 r = sqrt(x[0]**2 + x[1]**2 + x[2]**2) 
 #initial = as_vector( [conditional(r>Constant(30), Constant(-1), conditional(abs(x[2]) > Constant(10), Constant(-1), 1)), u_initial] )
 initial = as_vector( [1-2/(1+exp(8-r)), u_initial])
@@ -28,13 +28,14 @@ def test1(gridView):
     eps = Constant(1e-5, "eps")
 
     Gamma = Constant(0.5, "Gamma")
+    epsilonz = Constant(0.3, "Epsilonz")
+    Lsat = Constant(1.0, "L_sat")
+
     GammaVector = as_vector([1, 1, Gamma])
     GammaMatrix = as_matrix([[1, 0, 0], [0, 1, 0], [0, 0, Gamma]])
     epsilonxy = Constant(0.1, "Epsilonxy")
-    epsilonz = Constant(0.05, "Epsilonz")
     Lambda = Constant(3.0, "Lambda") #Wrong value but isn't numerically unstable unlike 3.0
     D = Constant(0.6267*Lambda.value, "D")
-    Lsat = Constant(1.0, "L_sat")
     phi = u[0]
     u_ = u[1]
 
@@ -78,21 +79,7 @@ def test1(gridView):
         + epsilonz * (2 * ygp * zgp / (inner(gradPhi, gradPhi) * sqrt(xgp * xgp + ygp * ygp) + eps)) * (-sin(2 * atan_2(sqrt(xgp**2 + ygp**2), safe_gradPhi2)))
 ,
           epsilonz * (-2 * sqrt(xgp * xgp + ygp * ygp) / (inner(gradPhi, gradPhi) + eps)) * (-sin(2 * atan_2(sqrt(xgp**2 + ygp**2), safe_gradPhi2)))
-    ])
-
-#     dA2dGP2 = as_matrix([[
-#         epsilonxy * 12 * ygp * (xgp * sin(6 * atan_2(ygp, safe_gradPhi0)) + 3 * ygp * cos(6 * atan_2(y, safe_gradPhi0)))/((xgp**2 + ygp**2)**2 + eps)
-        
-# ,
-#         epsilonxy * 6 * ((xgp**2 - ygp**2) * sin(6*atan_2(y, safe_gradPhi0)) + 6 * xgp * ygp * cos(6 * atan_2(y, safe_gradPhi0)))/((xgp**2 + ygp**2)**2 + eps)
-
-# ,       
-#         0
-
-#     ],[
-
-#     ]])
-    
+    ])  
 
     if True:
 
@@ -104,21 +91,6 @@ def test1(gridView):
         AAdtU = (A * A * D * dot(q * GammaMatrix * grad(u_), GammaMatrix * grad(v[1])) - 0.5 * Lsat * b * AAdtPhi(v[1]))
         
         form = (AAdtPhi(v[0]) + AAdtU) * dx
-                
-        # dtPhi = 1/(A*A)*(-fDash + Lambda * b * gDash * u_
-        #                 +0.5*div(GammaMatrix * (inner(gradPhi, gradPhi) * dAdGP + A*A*GammaMatrix*gradPhi)))
-
-        # dtPhi = (1/(A*A))*(-fDash + Lambda * gDash * u_
-        #                 +0.5 * (2 * dot(div(GammaMatrix * gradPhi) * gradPhi, 2 * A * dAdGP)
-        #                         + inner(gradPhi, gradPhi) * 2 * dot(grad(GammaMatrix * gradPhi) * dAdGP, dAdGP)
-        #                         + A * div(GammaMatrix * dAdGP)
-        #                         + 2 * A * inner((grad(GammaMatrix * gradPhi) * dAdGP), (GammaMatrix * gradPhi))
-        #                         + A*A * div(GammaMatrix * GammaMatrix * gradPhi))
-        #                         )
-
-        # dtU = (D * div(GammaMatrix * q * GammaMatrix * grad(u_)) - 0.5 * Lsat * b * dtPhi)
-        
-        # form = (dtPhi * v[0] + dtU * v[1])*dx
 
     else:
         dtPhi = lambda v_:  (v_/(A*A) * (-fDash + Lambda * gDash * u_)
@@ -132,6 +104,6 @@ def test1(gridView):
 
         massA = None
 
-    return -form, 100, 0.05, initial, None, massA
+    return -form, 10, 0.05, initial, None, massA
 
     
