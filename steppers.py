ca76c58170b9672a99d62ff4dc9ebf9873054fac
Harvey <tyrrellhs@gmail.com>
2025-02-27 23:34:14 +0000

    Updated Adaptive Grid

--- steppers.py
+++ steppers.py
@@ -486,14 +486,16 @@ if __name__ == "__main__":
     elif sysargs.problem=="Snowflake3D":
         from snowflakes3D import dimR, time, sourceTime, domain
         from snowflakes3D import test1 as problem
-        from dune.alugrid import aluConformGrid as leafGridView
+        #from dune.alugrid import aluConformGrid as leafGridView
+        from dune.alugrid import aluCubeGrid as leafGridView
         baseName = "Snowflake3D"
         order = 1
         if sysargs.adaptive:
             kwargs = {"grid": "adaptive"}
             def adaptGrid(u_h):
                 indicator = dot(grad(u_h[0]),grad(u_h[0]))
-                mark(indicator,0.1,0.01,0,10, markNeighbors = False)
+                #mark(indicator,0.1,0.01,0,18, markNeighbors = True)
+                mark(indicator,0.02,0.02,0,7, markNeighbors = False)
                 adapt(u_h)
     elif sysargs.problem=="Parabolic":
         from parabolicTest import dimR, time, sourceTime, domain
