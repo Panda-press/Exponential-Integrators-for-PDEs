6b2687cd397ae4d95b2e08f7e6d16e2c1345ddf7
Harvey <tyrrellhs@gmail.com>
2025-03-19 14:10:19 +0000

    Created Cahn Hilliard Problem

---
+++ cahnHilliard.py
@@ -0,0 +1,30 @@
+import numpy as np
+from ufl import *
+from dune.ufl import Space, Constant
+from dune.fem.function import gridFunction
+from parabolicTest import model
+
+dimR = 2
+time = Constant(0,"time")
+sourceTime = Constant(0,"sourceTime")
+domain = [0, 0], [1, 1], [60, 60]
+
+space = Space(2,dimRange=dimR)
+x,u,v,n = ( SpatialCoordinate(space), TrialFunction(space), TestFunction(space), FacetNormal(space) )
+
+
+def test2(gridView):
+    eps = Constant(0.01,"eps")
+    M = Constant(1, "M")
+    Fdash = u[0] * (u[0]**2 - 1)
+    a = (- M * inner(grad(u[1]), grad(v[0]))
+        + (Fdash * v[1] + eps*eps * inner(grad(u[0]), grad(v[1]))))
+        
+    @gridFunction(gridView,name="random",order=1)
+    def u0(x):
+        return 1.8*np.random.rand(1)[0] -0.9
+    np.random.seed(100)
+
+    return -a * dx, 1, 0.001, [u0,0], None, None
+
+
